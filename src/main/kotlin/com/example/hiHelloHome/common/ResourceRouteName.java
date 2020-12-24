242ecf8fd739e58c0806fe4ca4fb1db0d1b9afda
최창민 <ccmain@wemakeprice.com>
2020-12-24 16:50:11 +0900

    Revert "temp image upload"
    This reverts commit a16020955447bfd69309eb5a450bd6d9d28c848d.

--- src/main/kotlin/com/example/hiHelloHome/common/ResourceRouteName.java
+++
@@ -1,6 +0,0 @@
-package com.example.hiHelloHome.common;
-
-public class ResourceRouteName {
-    public static final String LOCAL = "local";
-    public static final String UPLOAD_AWS = "upload-aws";
-}
--- src/main/kotlin/com/example/hiHelloHome/common/constants/Image.kt
+++
@@ -1,9 +0,0 @@
-package com.example.hiHelloHome.common.constants
-
-object Image {
-    const val URI_PREFIX = "review"
-    const val UPLOAD_IMAGE_KEY = "image_key"
-    const val UPLOAD_IMAGE_VALUE = "ReviewImage"
-    const val UPLOAD_RESOURCE_KEY = "image_file"
-    const val UPLOAD_PATH = "/image"
-}
